17c0f17bc6b12412c7ef84103a7ce6f5cfb92f74
Dmitriy <dmitriyvalerievich24@mail.ru>
2021-03-20 20:55:15 +0300

    раздел 10 массивы task 19 min значение

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ let button = document.querySelector('button');
 
 
 button.onclick = () => {
-  ar =  [3, 0, 2, 6, 0, 1, 30, 1, 9, 0, 2, 0];
-    let max = ar[0];
+  ar =  [3, 1, 2, 6, 10, 1, 30, 1, 9, 40, 22, 10];
+    let min = ar[0];
     for (let i = 0; i < ar.length; i++) {
-      if (ar[i] > max) {
-         max = ar[i] + ' ';
+      if (ar[i] < min) {
+         min = ar[i];
       }
     }
-    out.innerHTML = 'max: ' + max;
+    out.innerHTML = 'min: ' + min;
   }
   
 
